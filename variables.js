98089453f07fc68e0785de0639b8d31c4772fa2f
Karthick_S <karthick712004@gmail.com>
2024-01-21 20:59:14 +0530

    second commit

---
+++ variables.js
@@ -0,0 +1,10 @@
+a="100"
+console.log("1.",a)
+var x='karthick'
+console.log("Value of x",x)
+var y=10
+console.log("Value of y",y)
+let z=true
+console.log("Boolean z is",z)
+const d="123456789"
+console.log("Value od d",d)
