4a2a431b532f585c7ec08064e9668d60d251c201
Karthick_S <karthick712004@gmail.com>
2024-01-22 09:11:20 +0530

    Jcommit01

---
+++ operators.js
@@ -0,0 +1,41 @@
+// //Assignment Operator
+// var a=10
+// console.log('Assignment operation output:',a)
+// //Arithmetic operator
+// var aA=10
+// var bB=20
+// console.log('Addition output:',aA+bB)
+// console.log('Subtratcion output:',aA-bB)
+// console.log('Multiplication output:',aA*bB)
+// console.log('Division output:',bB/aA)
+// console.log("Datatype of aA is:",typeof aA)
+// let c='a'
+// let d='becd'
+// console.log('String Concatination',c+d)
+// console.log("Logical operator && output:",aA&&bB)
+// console.log("Logical operator || output:",aA&&bB)
+// console.log("Logical operator & output:",!aA)
+// console.log(30 && 330)
+// console.log(30 && 0)
+// console.log(0 && 40)
+// console.log(30 || 330)
+// console.log(30 || 0)
+// console.log(0 || 40)
+// console.log((100 || 40) && 0)
+// console.log(100 || 40 && 0)
+// console.log(!-10)
+// console.log(!10)
+
+//Relational Operators - <,>,<=,>=,===,!==,++,-- Output is either true or false.
+aa=10
+bb=20
+console.log("Relational Operator > is:",aa>bb)
+console.log("Relational Operator < is:",aa<bb)
+console.log("Relational Operator <= is:",aa<=bb)
+console.log("Relational Operator >= is:",aa>=bb)
+console.log("Relational Operator === is:",aa===bb)
+console.log("Relational Operator !== is:",aa!==bb)
+console.log("aa after increment:",aa++)
+console.log("aa after decrement:",aa--)
+console.log("bb after increment:",bb++)
+console.log("bb after decrement:",bb--)
