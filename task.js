9505b070a398828d9d8a69566a1d6eb64730ccad
Karthick_S <karthick712004@gmail.com>
2024-01-22 17:19:16 +0530

    Jcommit12

--- task.js
+++ task.js
@@ -1,1 +1,35 @@
-console.log("I will upload within 7.30")
+function ispwd(password)
+{
+    if(password==="karthick7167")
+    {
+        return 1
+    }
+    else
+    {
+        return 0
+    }
+}
+function Password(password)
+{
+    return new Promise(function(correct,incorrect)
+    {
+        setTimeout(function()
+        {
+            if(ispwd(password))
+            {
+                correct("Password is correct")
+            }
+            else
+            {
+                incorrect("Password incorrect")
+            }
+        },1000)
+    })
+}
+Password("karthick7167")
+.then(function (message) {
+    console.log(message);
+  })
+  .catch(function (message) {
+    console.log(message);
+  });
