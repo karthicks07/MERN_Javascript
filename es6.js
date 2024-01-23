7c74439cd5b65d09152bd9e0e62a9c9b6c2afb8f
Karthick_S <karthick712004@gmail.com>
2024-01-23 15:57:57 +0530

    commit

---
+++ es6.js
@@ -0,0 +1,18 @@
+// //ES6 -> ECMA Script 6 Starbdard
+// //1. Var, let, const => refer variable.js
+// //2. Arrow Function
+
+// var fun=()=>{
+//     console.log("hello")
+// }
+// fun()
+
+//3. Destructuring Operator
+var array=[10,20,30,40,50]
+var [a,b,c,d,e]=array
+console.log(a,b,c,d,e)
+
+//4. Spread Operator
+var array1=[10,20,30,40,50]
+var a1=[...array,60,70]
+console.log(a1)
